4f916c3d60b25e3c3ec1636a6509477f27973323
mades eladrian <mades.eladrian@gmail.com>
2022-07-25 12:21:35 -0300

    move body parameters to a RemoteAuthenticationParams

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -3,6 +3,7 @@ import '../../domain/params/params.dart';
 
 import '../http/http.dart';
 import '../models/models.dart';
+import '../params/params.dart';
 
 class RemoteAuthentication {
   final String url;
@@ -11,7 +12,7 @@ class RemoteAuthentication {
   RemoteAuthentication({required this.url, required this.httpClient});
 
   Future<AccountEntity> auth(AuthenticationParams params) async {
-    final body = {"username": params.email, "password": params.password};
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     final httpResponse = await httpClient.request(url: url, method: 'post', body: body);
     return RemoteAccountModel.fromJson(httpResponse).toEntity();
   }
