dd81c836682b0e7d1cbda84e86c1928b79390090
mades eladrian <mades.eladrian@gmail.com>
2022-07-25 23:22:59 -0300

    ensure EmailValidation returns error if value is empty

---
+++ lib/validation/validators/required_field_validation.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+import '../protocols/protocols.dart';
+
+class RequiredFieldValidation extends Equatable implements FieldValidation {
+  @override
+  final String field;
+ 
+  @override
+  List get props => [field];
+
+  const RequiredFieldValidation(this.field);
+ 
+  @override
+  String? validate(Map input) {
+    return input[field]?.isNotEmpty == true ? null : 'Campo obrigatório';
+  }
+}
---
+++ test/validation/validators/required_field_validation_test.dart
@@ -0,0 +1,20 @@
+import 'package:test/test.dart';
+import 'package:ignis_insight/validation/validators/validators.dart';
+
+void main() {
+  late RequiredFieldValidation sut;
+  
+  setUp(() {
+    sut = const RequiredFieldValidation('any_field');
+  });
+ 
+  test('1 - Should return null if value is not empty', () async {
+    final error = sut.validate({'any_field': 'any_value'});
+    expect(error, null);
+  });
+
+  test('2 - Should return error if value is empty', () async {
+    final error = sut.validate({'any_field': ''});
+    expect(error, 'Campo obrigatório');
+  });
+}
