79f95f52262ed981bafc30737d40f1c2a3ff9c49
mades eladrian <mades.eladrian@gmail.com>
2022-07-27 11:46:50 -0300

    GetxLoginPresenter implements LoginPresenter

--- lib/presentation/presenters/getx_login_presenter.dart
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -5,13 +5,15 @@ import '../../domain/helpers/helpers.dart';
 import '../../domain/usecases/usecases.dart';
 
 import '../../ui/helpers/helpers.dart';
+import '../../ui/pages/pages.dart';
 
 import '../helpers/helpers.dart';
 import '../mixins/mixins.dart';
 import '../protocols/protocols.dart';
 
 class GetxLoginPresenter extends GetxController 
-with FormManager, LoadingManager, NavigationManager, UIErrorManager {
+with FormManager, LoadingManager, NavigationManager, UIErrorManager 
+implements LoginPresenter {
   final Authentication authentication;
   final SaveCurrentAccount saveCurrentAccount;
   final Validation validation;
@@ -22,7 +24,9 @@ with FormManager, LoadingManager, NavigationManager, UIErrorManager {
   final _emailError = Rx<UIError?>(null);
   final _passwordError = Rx<UIError?>(null);
 
+  @override
   Stream<UIError?> get emailErrorStream => _emailError.stream;
+  @override
   Stream<UIError?> get passwordErrorStream => _passwordError.stream;
 
   GetxLoginPresenter({
@@ -51,18 +55,21 @@ with FormManager, LoadingManager, NavigationManager, UIErrorManager {
       && _password != null;
   }
 
+  @override
   void validateEmail(String email) {
     _email = email;
     _emailError.value = _validateField(field: 'email');
     _validateForm();
   }
 
+  @override
   void validatePassword(String password) {
     _password = password;
     _passwordError.value = _validateField(field: 'password');
     _validateForm();
   }
 
+  @override
   Future<void> auth() async {
     try {
       mainError = null;
@@ -82,10 +89,12 @@ with FormManager, LoadingManager, NavigationManager, UIErrorManager {
     }
   }
 
+  @override
   void support() {
     navigateTo = '/support';
   }
 
+  @override
   void recoverPassword() {
     navigateTo = '/recoverPassword';
   }
