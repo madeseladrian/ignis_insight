af82320fecd5b10644f38d179317a161277998d3
mades eladrian <mades.eladrian@gmail.com>
2022-07-27 12:03:38 -0300

    ensure ValidationComposite returns null if all validations returns null

---
+++ test/main/composites/validation_composite_test.dart
@@ -0,0 +1,27 @@
+import 'package:test/test.dart';
+
+import 'package:ignis_insight/main/composites/composites.dart';
+import '../../validation/mocks/mocks.dart';
+
+void main() {
+  late ValidationComposite sut;
+  late FieldValidationSpy validation1;
+  late FieldValidationSpy validation2;
+  late FieldValidationSpy validation3;
+
+  setUp(() {
+    validation1 = FieldValidationSpy();
+    validation1.mockFieldName('other_field');
+    validation2 = FieldValidationSpy();
+    validation3 = FieldValidationSpy();
+    sut = ValidationComposite([validation1, validation2, validation3]);
+  });
+ 
+  test('1 - Should return null if all validations returns null', () async {
+    final formData = {'any_field': 'any_value'};
+    
+    final error = sut.validate(field: 'any_field', input: formData);
+
+    expect(error, null);
+  });
+}
